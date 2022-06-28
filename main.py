9e119752d349e5be036a0aba61389dd6818c0ad2
dipak <ramoliyadipak333@gmail.com>
2022-06-29 00:43:34 +0530

    start project

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
+def printRange(my_range):
+    print("please enter value between 1 to " + str(my_range))
 
 
+mylist = []
 
 
+# This function take input for given range and store it in list
+def getInputInRange(my_range):
+    for i in range(0, my_range):
+        new_val = int(input("enter val " + str(i + 1) + ":- "))
+        mylist.append(new_val)
 
-def printRange(myRange):
-    print("please enter value between 1 to " + str(myRange))
-
-    def getInputInRange(myRange):
-        {
+    return True
 
 
+#  This function check is input type is int
+def isIntValue(val):
+    True  # type(int(val)) == int
 
-         }
 
 while True:
-    firstRange = int(input("enter val :- "))
+    firstRange = input("enter val :- ")
+    firstRange = int(firstRange)
     if firstRange in range(1, 21):
-        getInputInRange(myRange)
+        if getInputInRange(firstRange):
+            print("List:- \n")
+            print(mylist)
+            break
     else:
         printRange(20)
-
-
-
-
